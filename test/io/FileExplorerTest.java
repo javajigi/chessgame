067e1e04c7e05d4d25cb97a1621651e68b459030
javajigi <javajigi@gmail.com>
2013-08-13 11:24:23 +0900

    웹 서버 기본 템플릿과 IO 실습 예제 추가함.

---
+++ test/io/FileExplorerTest.java
@@ -0,0 +1,10 @@
+package io;
+
+import junit.framework.TestCase;
+
+public class FileExplorerTest extends TestCase {
+	public void testExplorer() throws Exception {
+		FileExplorer explorer = new FileExplorer();
+		explorer.showTree(".");
+	}
+}
