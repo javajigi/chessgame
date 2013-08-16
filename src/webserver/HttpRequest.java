507b28de327bd8403ae9fe817f8aa3f4feed1fd1
javajigi <javajigi@gmail.com>
2013-08-16 18:44:24 +0900

    8월 16일 구현 부분까지 구현 완료함.

--- src/webserver/HttpRequest.java
+++ src/webserver/HttpRequest.java
@@ -5,15 +5,40 @@ import java.util.Map;
 
 public class HttpRequest {
 	public String parseRequestUrl(String header) {
-		return null;
+		String[] values = header.split(" ");
+		return values[1];
 	}
 
 	public String parseRequestPath(String requestUrl) {
-		return null;
+		int index = requestUrl.indexOf("?");
+		if (index == -1) {
+			return requestUrl;
+		}
+		return requestUrl.substring(0, index);
 	}
 
 	public Map<String, String> parseParameters(String requestUrl) {
-		return null;
+		int index = requestUrl.indexOf("?");
+		if (index == -1) {
+			return new HashMap<String, String>();
+		}
+		
+		String value = requestUrl.substring(index + 1);
+		if (!value.contains("&")) {
+			return new HashMap<String, String>();
+		}
+		
+		Map<String, String> parameters = new HashMap<String, String>();
+		String[] keyValues = value.split("&");
+
+		for (String keyValue : keyValues) {
+			String[] splitedKeyValue = keyValue.split("=");
+			if (splitedKeyValue.length == 2) {
+				parameters.put(splitedKeyValue[0], splitedKeyValue[1]);
+			}
+		}
+		
+		return parameters;
 	}
 	
 }
--- src/webserver/RequestHandler.java
+++ src/webserver/RequestHandler.java
@@ -2,56 +2,83 @@ package webserver;
 
 import static util.Constants.NEW_LINE;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler extends Thread {
-    private final static Logger log = Logger.getLogger(RequestHandler.class
-            .getName());
+	private final static Logger log = Logger.getLogger(RequestHandler.class
+			.getName());
 
-    private static final String DEFAULT_WEBAPPS_DIR = "./webapps";
+	private static final String DEFAULT_WEBAPPS_DIR = "./webapps";
 
-    private Socket connection;
+	private Socket connection;
 
-    public RequestHandler(Socket connection) {
-        this.connection = connection;
-    }
+	public RequestHandler(Socket connection) {
+		this.connection = connection;
+	}
 
-    public void run() {
-        log.log(Level.INFO, "WebServer Thread Created!");
-        InputStream is = null;
-        OutputStream os = null;
-        
-        try {
-            is = connection.getInputStream(); // 사용자 요청
-            os = connection.getOutputStream(); // 사용자 응답
+	public void run() {
+		log.log(Level.INFO, "WebServer Thread Created!");
+		BufferedReader br = null;
+		DataOutputStream dos = null;
 
-            // 구현 
+		try {
+			br = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			dos = new DataOutputStream(connection.getOutputStream());
 
-            connection.close();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-    
-    private void responseHtmlOk(DataOutputStream dos, long contentsSize)    throws IOException {
-        responseOk(dos, contentsSize, "text/html");
-    }
+			String header = br.readLine();
+			
+			if (header==null || header.equals("")) {
+				return;
+			}
+			
+			HttpRequest request = new HttpRequest();
+			String requestUrl = request.parseRequestUrl(header);
+			String requestPath = request.parseRequestPath(requestUrl);
+			System.out.println("requestPath : " + requestPath);
 
-    private void responseJavascriptOk(DataOutputStream dos, long contentsSize) throws IOException {
-        responseOk(dos, contentsSize, "text/javascript");
-    }
+			File file = new File(DEFAULT_WEBAPPS_DIR + requestPath);
+			if (file.exists()) {
+				responseHtmlOk(dos, file.length());
+				FileInputStream fis = new FileInputStream(file);
+				int data = fis.read();
+				while (data != -1) {
+					dos.write(data);
+					data = fis.read();
+				}
+				fis.close();
+			}
 
-    private void responseOk(DataOutputStream dos, long contentsSize, String contentType)
-            throws IOException {
-        dos.writeBytes("HTTP/1.0 200 Document Follows " + NEW_LINE);
-        dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8" + NEW_LINE);
-        dos.writeBytes("Content-Length: " + contentsSize + NEW_LINE);
-        dos.writeBytes(NEW_LINE);
-    }
+			connection.close();
+		} catch (IOException e) {
+			log.log(Level.SEVERE, e.getMessage());
+		}
+	}
+
+	private void responseHtmlOk(DataOutputStream dos, long contentsSize)
+			throws IOException {
+		responseOk(dos, contentsSize, "text/html");
+	}
+
+	private void responseJavascriptOk(DataOutputStream dos, long contentsSize)
+			throws IOException {
+		responseOk(dos, contentsSize, "text/javascript");
+	}
+
+	private void responseOk(DataOutputStream dos, long contentsSize,
+			String contentType) throws IOException {
+		dos.writeBytes("HTTP/1.0 200 Document Follows " + NEW_LINE);
+		dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8"
+				+ NEW_LINE);
+		dos.writeBytes("Content-Length: " + contentsSize + NEW_LINE);
+		dos.writeBytes(NEW_LINE);
+	}
 }
