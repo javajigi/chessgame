011272473cce3081b364d9354aad585cd8bf1c99
javajigi <javajigi@gmail.com>
2013-08-16 19:14:10 +0900

    추가 기능 구현함.

--- test/webserver/HttpRequestTest.java
+++ test/webserver/HttpRequestTest.java
@@ -13,41 +13,33 @@ public class HttpRequestTest extends TestCase {
 	}
 	
 	public void testRequestUrl() throws Exception {
-		String header = "GET /move?source=a1&target=a2 HTTP/1.1";
+		String header = "GET index.html HTTP/1.1";
 		String requestUrl = request.parseRequestUrl(header);
-		assertEquals("/move?source=a1&target=a2", requestUrl);
+		assertEquals("index.html", requestUrl);
 	}
 	
 	public void testRequestPath() throws Exception {
-		String requestUrl = "/move?source=a1&target=a2";
+		String requestUrl = "index.html?source=a1&target=a2";
 		String requestPath = request.parseRequestPath(requestUrl);
-		assertEquals("/move", requestPath);
+		assertEquals("index.html", requestPath);
 	}
 	
 	public void testGetParameters() throws Exception {
-		String requestUrl = "/move?source=a1&target=a2";
+		String requestUrl = "index.html?/move?source=a1&target=a2";
 		Map<String, String> parameters = request.parseParameters(requestUrl);
 		assertEquals("a1", parameters.get("source"));
 		assertEquals("a2", parameters.get("target"));
 	}
 	
 	public void testGetParametersException1() throws Exception {
-		String requestUrl = "/move?";
+		String requestUrl = "index.html?";
 		Map<String, String> parameters = request.parseParameters(requestUrl);
 		assertEquals(0, parameters.size());
 	}
 	
 	public void testGetParametersException2() throws Exception {
-		String requestUrl = "/move?source=&target=";
+		String requestUrl = "index.html?source=&target=";
 		Map<String, String> parameters = request.parseParameters(requestUrl);
 		assertEquals(0, parameters.size());
-	}
-	
-	public void testInitialize() throws Exception {
-		String header = "GET /move?source=a1&target=a2 HTTP/1.1";
-		request = new HttpRequest(header);
-		assertEquals("/move", request.getRequestPath());
-		assertEquals("a1", request.getParameter("source"));
-		assertEquals("a2", request.getParameter("target"));
 	}
 }
